8f5254ddfd81fdf4c2a4b9ce9c8ed2e931a61d15
Mipa <mp1999my@gmail.com>
2024-12-09 19:09:27 +0900

    fixed menu button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,11 @@
 // サイドメニューの開閉を制御する関数
 function toggleMenu() {
-    var sideMenu = document.querySelector('.sidebar');  // サイドメニューを取得
-    sideMenu.classList.toggle('active');  // 'active' クラスをトグル（開閉）
+    var sideMenu = document.querySelector('.sidebar'); // サイドバーを取得
+    var menuButton = document.querySelector('.menu-button'); // メニューボタンを取得
+
+    // メニューの開閉をトグル
+    sideMenu.classList.toggle('active');
+
+    // メニューボタンが消えないように、常に表示状態を維持
+    menuButton.style.display = 'block';
 }
